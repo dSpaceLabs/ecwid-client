3b5915ed270074cb592d8fa41a83ea03bd2cc083
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2017-06-12 18:01:35 -0400

    more documentation and making sure tests will pass

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -6,4 +6,8 @@ use Dspacelabs\Component\Ecwid\Client;
 
 class ClientTest extends \PHPUnit_Framework_TestCase
 {
+    public function testConstruct()
+    {
+        $this->markTestIncomplete();
+    }
 }
