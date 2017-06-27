f8f0c8214429b2634e30018414c5f930dfd939a9
Joshua Estes <JoshuaEstes@users.noreply.github.com>
2017-06-26 21:41:15 -0400

    spacing

--- src/Client.php
+++ src/Client.php
@@ -76,7 +76,7 @@ class Client extends BaseClient
                     )
                 )
             );
-        $request = (new Request())->withMethod('POST')->withUri($uri);
+        $request  = (new Request())->withMethod('POST')->withUri($uri);
         $response = $this->sendWithRequest($request);
 
         return json_decode($response->getBody()->getContents(), true);
